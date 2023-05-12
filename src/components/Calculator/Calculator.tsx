bce3c068be667ecfd1e653dccdbf1761379ba1a6
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:59:55 +0300

    add history

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,8 +1,18 @@
-import { Button, Col, Input, Layout, Row } from 'antd'
+import { Button, Col, Input, Layout, Row, Typography } from 'antd'
+import { useState } from 'react'
 
 export const Calculator = () => {
   const numberActions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
   const mathActions: string[] = ['+', '-', '*', '/', '%']
+
+  const [historyList, setHistoryList] = useState<string[]>([])
+
+  const addHistoryItem = (historyItem: string) =>
+    setHistoryList([...historyList, historyItem])
+
+  const goAction = (action: string | number) => {
+    addHistoryItem(`${action}`)
+  }
 
   return (
     <Layout
@@ -14,13 +24,22 @@ export const Calculator = () => {
       <Layout.Content>
         <Row gutter={[0, 8]}>
           <Col span={24}>
+            <Typography>
+              <p>{historyList.join(' ')}</p>
+            </Typography>
+          </Col>
+          <Col span={24}>
             <Input size={'large'} value={3} disabled />
           </Col>
           <Col span={20}>
             <Row gutter={[8, 8]}>
               {numberActions.map((n) => (
                 <Col span={7} key={n}>
-                  <Button shape={'circle'} size={'large'}>
+                  <Button
+                    onClick={() => goAction(n)}
+                    shape={'circle'}
+                    size={'large'}
+                  >
                     {n}
                   </Button>
                 </Col>
@@ -31,7 +50,11 @@ export const Calculator = () => {
             <Row gutter={[8, 8]}>
               {mathActions.map((n) => (
                 <Col span={24} key={n}>
-                  <Button shape={'circle'} size={'large'}>
+                  <Button
+                    onClick={() => goAction(n)}
+                    shape={'circle'}
+                    size={'large'}
+                  >
                     {n}
                   </Button>
                 </Col>
