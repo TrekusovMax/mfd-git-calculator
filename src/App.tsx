385a0c2273dbd5fbdcb2cd660a5fae6c6a34f8f7
taraswww777 <taraswww777@mail.ru>
2023-05-12 16:07:36 +0300

    fix package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
-import {ConfigProvider} from 'antd';
-import {ANT_THEME_CONFIG} from 'src/constants/configTheme.ts';
-import {CommonLayout} from 'src/components/CommonLayout';
+import { ConfigProvider } from 'antd'
+import { ANT_THEME_CONFIG } from 'src/constants/configTheme.ts'
+import { CommonLayout } from 'src/components/CommonLayout'
 
 function App() {
-
   return (
     <ConfigProvider theme={ANT_THEME_CONFIG}>
       <CommonLayout />
